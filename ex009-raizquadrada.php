495474955574d33cb5fd2b9d635ab674ecf7d810
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-16 23:01:41 -0300

    exercício 009 concluído com sucesso!

---
+++ ex009-raizquadrada.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Raiz Quadrada</title>
+</head>
+<body>
+    <p>Informe um valor para verificar sua Raiz Quadrada:</p>
+    <form method="GET" action="ex009-raizquadrada.php">
+        Número: <input type="number" name="num" required>
+        <input type="submit" value="verificar">
+        <?php
+            $num = $_GET["num"];
+            $quad = sqrt($num);
+            echo "A raiz quadrada de $num é $quad";
+        ?>
+    </form>
+</body>
+</html>
