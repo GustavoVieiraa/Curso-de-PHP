cdf4344a13ce6d6d1313f217b3fae46450146918
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-17 15:05:22 -0300

    aula10 concluída com sucesso!

---
+++ aula10.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura Condicional Switch</title>
+</head>
+<body>
+    <?php 
+    
+    /* Nessa aula vamos aprender a utilizar estrutura de condição Switch:
+    temos um exemplo de como ela funciona:
+    
+    ex. 
+        switch ($esc) {
+            case 1:
+                echo "A";
+                break;
+            case 2:
+                echo "B";
+                break;
+            case 3: 
+                echo "C";
+                break;
+            default:
+                echo "ERRO!";
+        }
+    Então essa estrutura, ela vai verificar um valor e vai retornar para gente um resultado
+    com base no valor escolhido pelo usuário informado por uma varíavel por exemplo.
+    Com vase nesse valor informado, ela vai verificar em seus case se encontra algum valor
+    igual ao informado pela entrada e retornar o valor que tem dentro do case, e para retornar
+    somente aquele valor do case ela ira por fim quebrar a linha utilizando o Break.
+    
+    Caso não encontre nenhum valor igual ao informado na entrada, ela vai chegar ao default e retornar
+    o valor que há dentro de default.
+    
+    */
+    
+    ?>
+</body>
+</html>
