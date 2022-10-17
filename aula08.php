d8a7fc237bdc62f96528df692a788b1aa6477256
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-16 21:50:18 -0300

    Aula08 integração de PHP com HTML

---
+++ aula08.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integração de PHP com HTML5</title>
+</head>
+<body>
+    <?php 
+    
+    /*
+    
+    <form method="get" action="nomeparaondevaiserenviadoosdados.php"> 
+        <input type="text" name="nomevalor"/>
+        <input type="submit" value="Calcular/>"
+    </form>
+    
+    */
+
+    ?>
+</body>
+</html>
