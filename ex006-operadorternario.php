467f6b390f4f2aa6922f37eba10a93854610c296
Gustavo Vieira <gustavovieira292020@gmail.com>
2022-10-17 00:32:08 -0300

    correções

--- ex006-operadorternario.php
+++ ex006-operadorternario.php
@@ -12,7 +12,7 @@
     $nota2 = $_GET["n2"];
     $m = ($nota1+$nota2)/2;
     echo "<h1>Com as notas $nota1 e $nota2 a sua média foi $m</h1> <br>";
-    $r = $m>=7?"APROVADO!":"REPROVADO!"; # esse é um exemplo de operador unário
+    $r = $m>=7?"APROVADO!":"REPROVADO!"; # esse é um exemplo de operador ternario
     echo "<h1>Você foi $r</h1>";
     ?>
 </body>
